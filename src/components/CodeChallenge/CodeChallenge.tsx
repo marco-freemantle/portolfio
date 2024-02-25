eb9f057907888738a659635f9dd106ab47089d2b
Marco Freemantle <marcofreemantle@gmail.com>
2024-02-25 15:29:28 +0000

    initial commit

---
+++ src/components/CodeChallenge/CodeChallenge.tsx
@@ -0,0 +1,81 @@
+import "./CodeChallenge.css";
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import ConfettiExplosion from "react-confetti-explosion";
+
+function CodeChallenge() {
+  const challengeAnswer =
+    "function findLargestNumber(numbers) {\n    let largest = 0;\n    for (let i = 0; i < numbers.length; i++) {\n        if (numbers[i] > largest) {\n            largest = numbers[i];\n        }\n    }\n    return largest;\n}";
+  const initialState =
+    "function findLargestNumber(numbers) {\n    let largest = 0;\n    for (let i = 0; i < numbers.length; i++) {\n        if (numbers[i] < largest) {\n            largest = numbers[i];\n        }\n    }\n    return largest;\n}";
+  const [code, setCode] = useState(initialState);
+  const [isExploding, setIsExploding] = useState(false);
+  const [isWrongCode, setIsWrongCode] = useState(false);
+
+  const handleSubmit = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    if (code === challengeAnswer) {
+      setIsExploding(true);
+      setIsWrongCode(false);
+    } else {
+      setIsWrongCode(true);
+      // After 0.5 seconds, reset isWrongCode back to false
+      setTimeout(() => {
+        setIsWrongCode(false);
+      }, 500);
+    }
+  };
+
+  const handleReset = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    setIsExploding(false);
+    setCode(initialState);
+    setIsWrongCode(false);
+  };
+
+  return (
+    <>
+      <span style={{ color: "green", fontSize: "14px" }}>
+        {`//Fix the function for a confetti explosion! 😄🏆`}
+      </span>
+      <Form className={`styled-code-block ${isWrongCode ? "wiggle" : ""}`}>
+        <Form.Group>
+          <Form.Control
+            as="textarea"
+            rows={9}
+            size="sm"
+            className="styled-code-block"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+          />
+          <div
+            style={{
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            {isExploding && (
+              <ConfettiExplosion particleCount={100} duration={3000} />
+            )}
+          </div>
+        </Form.Group>
+      </Form>
+      <div className="button-container">
+        <button onClick={handleReset} className="reset-button">
+          Reset Code
+        </button>
+        <button onClick={handleSubmit} className="attempt-button">
+          Run Code
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default CodeChallenge;
