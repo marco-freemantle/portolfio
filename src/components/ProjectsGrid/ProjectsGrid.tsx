65891ba007eb3dee68a4b868542a500f95de5d60
Marco Freemantle <marcofreemantle@gmail.com>
2024-08-21 18:34:26 +0100

    Updating projects section

--- src/components/ProjectsGrid/ProjectsGrid.tsx
+++ src/components/ProjectsGrid/ProjectsGrid.tsx
@@ -1,19 +1,28 @@
 import "./ProjectsGrid.css";
-import TrymHome from "../../images/trym-website.png";
-import MultiplayerShooter from "../../images/MultiplayerShooter.jpeg";
-import BandBox from "../../images/BandBox.png";
-import Iris from "../../images/Iris.png";
-import CalorieTracker from "../../images/CalorieTracker.png";
+import MultiplayerShooter from "../../content/MultiplayerShooter.jpeg";
+import BandBox from "../../content/BandBox.png";
+import Iris from "../../content/Iris.png";
+import CalorieTracker from "../../content/CalorieTracker.png";
 import Tilt from "react-parallax-tilt";
+import ShooterVideo from "../../content/ShooterVideo.mp4";
+import RPGTestImage from "../../content/RPGTest.png";
+import RPGTestVid from "../../content/RPGTest.mp4";
 
 interface ProjectCardProps {
   imageSrc: string;
   title: string;
   description: string;
   link: string;
+  videoSrc: string;
 }
 
-function ProjectCard({ imageSrc, title, description, link }: ProjectCardProps) {
+function ProjectCard({
+  imageSrc,
+  title,
+  description,
+  link,
+  videoSrc,
+}: ProjectCardProps) {
   return (
     <Tilt
       tiltMaxAngleX={5}
@@ -22,7 +31,9 @@ function ProjectCard({ imageSrc, title, description, link }: ProjectCardProps) {
       transitionSpeed={2500}
       className="project-card"
     >
-      <img src={imageSrc} alt={title} className="project-card-image" />
+      <video className="project-card-video" controls poster={imageSrc}>
+        <source src={videoSrc} type="video/mp4" />
+      </video>
       <div className="project-title-description-container">
         <h2 className="project-card-title">{title}</h2>
         <a
@@ -44,27 +55,40 @@ function ProjectsGrid() {
       <h1 className="expertise-text">Things I'm Working On.</h1>
       <div className="row">
         <ProjectCard
-          imageSrc={BandBox}
-          title="BandBox"
-          description="Comprehensive business management platform targeted at working musicians."
+          imageSrc={RPGTestImage}
+          videoSrc={RPGTestVid}
+          title="RPG Test"
+          description="Experimenting with popular RPG mechanics for multiplayer games. Created in Unreal Engine 5 with C++."
           link="https://shorturl.at/cnstY"
-        />
-        <ProjectCard
-          imageSrc={CalorieTracker}
-          title="Calorie Tracker"
-          description="Manage diet recommendations and monitor daily calorie intake and expenditure with ease."
-          link="https://shorturl.at/bpNT0"
         />
       </div>
       <div className="row">
         <ProjectCard
           imageSrc={MultiplayerShooter}
+          videoSrc={ShooterVideo}
           title="Multiplayer Shooter"
           description="A fully networked multiplayer third-person shooter. Created in Unreal Engine 5 with C++."
           link="https://shorturl.at/cevxz"
         />
         <ProjectCard
+          imageSrc={BandBox}
+          videoSrc={"none"}
+          title="BandBox"
+          description="Comprehensive business management platform targeted at working musicians."
+          link="https://shorturl.at/cnstY"
+        />
+      </div>
+      <div className="row">
+        <ProjectCard
+          imageSrc={CalorieTracker}
+          videoSrc={"none"}
+          title="Calorie Tracker"
+          description="Manage diet recommendations and monitor daily calorie intake and expenditure with ease."
+          link="https://shorturl.at/bpNT0"
+        />
+        <ProjectCard
           imageSrc={Iris}
+          videoSrc={"none"}
           title="Iris - Voice Assistant"
           description="Similar to the beloved Alexa but a lot worse!"
           link="https://shorturl.at/dAFS9"
